8fb008f20853614e4891893e93427c34d4d55886
Laura Cabral <41152226+lauracabral@users.noreply.github.com>
2018-11-22 18:04:23 -0500

    Update LuminanceCheck.m
    Made modifications so there can be a different number of images to each category and fixed lum calculation.

--- LuminanceCheck.m
+++ LuminanceCheck.m
@@ -3,28 +3,25 @@
 scriptpth=fileparts(mfilename('fullpath'));
 categories={'Faces','Houses'};
 
-
-for n = 1:2
+lum={};
+for n=1:2
+   lum{n}=[];
     
-    filelist = dir(fullfile(scriptpth,categories{n},'*jpg'));
+   filelist = dir(fullfile(scriptpth,categories{n},'*jpg'));
     
-    % Arrays for output
-    totpix=nan(2,length(filelist));
-    lum=nan(2,length(filelist));
+   for i=1:length(filelist)
+       %read in the images
+       im = imread(fullfile(scriptpth,categories{n},filelist(i).name));
     
-    for i=1:length(filelist)
-        %read in the images
-        im = imread(fullfile(scriptpth,categories{n},filelist(i).name));
-    
-        im=double(im)/255; 
+       im=double(im)/255; 
                            
-        %luminance
-        lum(n,i)=mean(im(n,i));
+       %luminance
+       lumtest(n,i)=mean(im(:));
         
     end
     
 end
 
-[h p]=ttest2(lum(1,:),lum(2,:));
+[h p]=ttest2(lumtest(1,:),lumtest(2,:));
 fprintf('Faces vs. houses, lum p<%f\n',p);
         
